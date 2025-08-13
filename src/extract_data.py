3a691d84028a5ba612fa9bce019d6b687219f8cf
Callan <106984678+callantwy@users.noreply.github.com>
2025-08-13 20:49:51 +0100

    Refactor to add argparse support for csv input/output

--- src/extract_data.py
+++ src/extract_data.py
@@ -7,6 +7,7 @@ import csv
 import logging
 import re
 import sys
+import argparse
 
 # ------------------------
 # Configuration & Logging
@@ -96,19 +97,28 @@ def write_csv(output_file, data):
 # ------------------------
 # Main Processing
 # ------------------------
-if __name__ == '__main__':
-    input_file = 'data.csv'
-    output_file = 'clean_data.csv'
-
+def process_file(input_file, output_file):
     data = read_csv(input_file)
     if data:
-        clean_data = []
+        clean_rows = []
+        invalid_count = 0
         for row in data:
             if validate_row(row):
-                clean_data.append(transform_row(row))
-        write_csv(output_file, clean_data)
+                clean_rows.append(transform_row(row))
+            else:
+                invalid_count += 1
+
+        write_csv(output_file, clean_rows)
+        logging.info(f'Processing complete, {len(clean_rows)} valid rows, {invalid_count} invalid rows.')
 
 # ------------------------
 # CLI Entrypoint
 # ------------------------
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Clean and validate sales data.")
+    parser.add_argument("input", help="Path to input csv file")
+    parser.add_argument("--output", default="clean_data.csv", help="Path to save clean data")
+    #parser.add_argument("--errors", default="error_data.json", help="Path to save invalid data")
+    args = parser.parse_args()
 
+    process_file(args.input, args.output)
